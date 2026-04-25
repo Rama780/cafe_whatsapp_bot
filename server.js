1631e5a6354ec0a97e22f321738b1a4ce722442e
I Gede Putu Ramawan <rama@Is-MacBook-Pro.local>
2026-04-25 10:50:35 +0800

    cafe fix server2

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const OpenAI = require("openai");
+const fetch = require("node-fetch");
 
 const app = express();
 app.use(express.json());
